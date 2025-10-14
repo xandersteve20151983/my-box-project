05c1f66279bfd647a53d44d761cc6696a9b7d2b3
xandersteve20151983 <stevelxw1983@gmail.com>
2025-10-14 21:34:02 +1100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,6 @@
-// app/page.js
+// no "use client" here
 import PalletisationPanel from "./components/pallets/PalletisationPanel.client";
 
-const ffLength = 393;
-const ffWidth  = 208;
-
-export default function Home() {
-  return (
-    <main className="p-6 space-y-4">
-      <h1 className="text-lg font-semibold">Home</h1>
-
-      <section className="border rounded p-4">
-        <PalletisationPanel ffLength={ffLength} ffWidth={ffWidth} />
-      </section>
-    </main>
-  );
+export default function Page() {
+  return <PalletisationPanel />;
 }
